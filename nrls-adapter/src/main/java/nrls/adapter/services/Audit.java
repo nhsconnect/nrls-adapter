b34e8e54def60919a0f00a7d1ae71e8378aaf454
James Cox <james.cox@answerdigital.com>
2018-08-14 11:05:49 +0100

    Added date to audit file name so that if the same transaction is run through twice there is an audit of both requests.

--- nrls-adapter/src/main/java/nrls/adapter/services/Audit.java
+++ nrls-adapter/src/main/java/nrls/adapter/services/Audit.java
@@ -1,5 +1,7 @@
 package nrls.adapter.services;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
 import nrls.adapter.enums.RequestType;
 import nrls.adapter.model.AuditEntity;
@@ -16,9 +18,11 @@ public class Audit {
     private String auditPathProvider;
     
     private HashMap<String, AuditEntity> auditEntities;
+    private SimpleDateFormat simpleDateFormat;
 
     public Audit() {
         auditEntities = new HashMap<>();
+        simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss-SSS");
     }
 
     // Returns the existing audit entry or creates a new one if one does not exist already
@@ -43,12 +47,14 @@ public class Audit {
             // Start by removing the audit entity from the hashmap so that the memory can be cleaned up by the garbage collector
             auditEntities.remove(id);
             
+            auditEntity.setDateLogged(new Date()); // Set the audit date to todays date
+            
             // Store the audit entry to a file
             String filePath;
             if(type == RequestType.CONSUMER){
-                filePath = auditPathConsumer + auditEntity.getNhsNumber() + "_Consumer_" + auditEntity.getTransactionId() + ".xml";
+                filePath = auditPathConsumer + auditEntity.getNhsNumber() + "_Consumer_" + simpleDateFormat.format(auditEntity.getDateLogged()) + "_" + auditEntity.getTransactionId() + ".xml";
             } else {
-                filePath = auditPathProvider + auditEntity.getNhsNumber() + "_Provider_" + auditEntity.getTransactionId() + ".xml";
+                filePath = auditPathProvider + auditEntity.getNhsNumber() + "_Provider_" + simpleDateFormat.format(auditEntity.getDateLogged()) + "_" + auditEntity.getTransactionId() + ".xml";
             }
             return FileHelper.writeObjectToFileAsXML(filePath, auditEntity);
         }
