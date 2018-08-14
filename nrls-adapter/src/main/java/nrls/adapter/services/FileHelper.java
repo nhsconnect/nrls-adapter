a2ed362afd0f2c4de9fc77f6bfcb97ff5185fb1e
James Cox <james.cox@answerdigital.com>
2018-08-14 10:35:38 +0100

    Updated file helper to create DIR if it does not exist and return a status as a boolean

--- nrls-adapter/src/main/java/nrls/adapter/services/FileHelper.java
+++ nrls-adapter/src/main/java/nrls/adapter/services/FileHelper.java
@@ -1,6 +1,7 @@
 package nrls.adapter.services;
 
 import com.thoughtworks.xstream.XStream;
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import org.jboss.logging.Logger;
@@ -9,15 +10,20 @@ public class FileHelper {
 
     private static final Logger LOG = Logger.getLogger(FileHelper.class);
     
-    public static void writeObjectToFileAsXML(String filePath, Object object) {
+    public static boolean writeObjectToFileAsXML(String filePath, Object object) {
         
+        boolean success = true;
         FileWriter fileWriter = null;
+        
         try {
-            fileWriter = new FileWriter(filePath);
+            File file = new File(filePath);
+            file.getParentFile().mkdirs();
+            fileWriter = new FileWriter(file);
             XStream xstream = new XStream();        // Using 'XStream' as it produces more readable XML output than JAXB and is quicker that XMLEncoder
             xstream.toXML(object, fileWriter);
         } catch (IOException ex) {
             LOG.error("Error converting object to XML when saving to file: " + ex.getMessage());
+            success = false;
         } finally {
             try {
                 if (null != fileWriter) {
@@ -25,8 +31,11 @@ public class FileHelper {
                 }
             } catch (IOException fileCloseEx) {
                 LOG.error("Error closing FileWriter after writing xml to file: " + fileCloseEx.getMessage());
+                success = false;
             }
         }
+        
+        return success;
     }
 
 }
